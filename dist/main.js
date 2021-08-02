2d24da78a5aecf0de14069bd65873014905d6a58
Jon Garrity <jonnygarrity@gmail.com>
2021-08-02 18:05:16 -0500

    gitIgnore updated and cache removed.

--- dist/main.js
+++
@@ -1,1 +0,0 @@
-console.log("I've changed this webpack!");
--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log("I've changed this webpack!");
+import './style.css';
+// import MenuPicture from './menuPicture.jpg';
+
+const initializePage = (function() {
+    console.log("I've been immediately invoked again!");
+
+    const banner = document.getElementById('banner');
+    const servicesContainer = document.getElementById('servicesContainer');
+    const menuPicDiv = document.createElement('div');
+    menuPicDiv.id = 'menuPicDiv';
+    servicesContainer.appendChild(menuPicDiv);
+    
+    // const menuPic = new Image();
+    // menuPic.src = MenuPicture;
+    
+    // menuPicDiv.style.background = menuPicture;
+    
+})();
